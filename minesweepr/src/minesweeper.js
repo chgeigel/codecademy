5b1b13a110f5d03a213519ada3f3fb46d1b02693
Chris Geigel <chris.geigel@autozone.com>
2018-09-28 10:39:10 -0500

    Day 11 & 12

--- minesweepr/src/minesweeper.js
+++ minesweepr/src/minesweeper.js
@@ -1,14 +1,45 @@
 const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join('|'));
-  console.log(board[1].join('|'));
-  console.log(board[2].join('|'));
+  console.log(board.map(row => row.join(' | ')).join('\n'));
 }
-const board = [ [' ' , ' ' , ' '] ,
-                [' ' , ' ' , ' '] ,
-                [' ' , ' ' , ' '] ];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for(let i = 0; i < numberOfRows; i++) {
+    const row = [];
+    for(let j=0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
 
-printBoard(board);
-board[0][1]='1';
-board[2][2]='B';
-printBoard(board);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for(let i = 0; i < numberOfRows; i++) {
+    const row = [];
+    for(let j=0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced = 0;
+
+  while ( numberOfBombsPlaced < numberOfBombs ) {
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+    if ( board[randomRowIndex][randomColumnIndex] == null ) {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
+  }
+
+  return board;
+};
+const playerBoard = generatePlayerBoard(3,4);
+const bombBoard = generateBombBoard(3,4,5);
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+printBoard(bombBoard);
